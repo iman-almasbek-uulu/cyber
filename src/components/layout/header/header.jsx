c5d07d272beabc93fc12fe58e865ba11d6ae3d57
Iman Almasbek uulu <iman.almasbekuulu.kk@gmail.com>
2024-10-18 16:27:28 +0600

    ggds

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -9,7 +9,6 @@ const Header = () => {
     return (
         <header className={headerClass}>
             <div className={`container ${s.containers}`}>
-
                 <NavLink to="/" className={s.logo}>
                     <img src="/images/logo.svg" alt="" />
                 </NavLink>
@@ -18,7 +17,7 @@ const Header = () => {
                     <NavLink to='/community' className={s.item}>Мероприятие</NavLink>
                     <NavLink to="/contact" className={s.item}>Контакты</NavLink>
                 </nav>
-                <btn className={`btn btn-info text-center text-white ${s.close_nav}`}><img src='/images/call.png' /> Позвонить нам</btn>
+                <a href="tel:+996701893489" className={`btn btn-info text-center call text-white ${s.close_nav} ${s.call}`}><img src='/images/call.png' /> Позвонить нам</a>
                 <img onClick={() => setShow(!show)} src="images/menu.png" className={!show ? s.menu  : s.n} alt="" />
                 <img onClick={() => setShow(!show)} src="images/close.png" className={show ? s.close : s.n} alt="" />
                 <div className={ show ? s.burger_block : s.n}>
@@ -26,9 +25,9 @@ const Header = () => {
                         <NavLink onClick={() => setShow(!show)} to="/news" className={s.item}>Новости</NavLink>
                         <NavLink onClick={() => setShow(!show)} to="/community" className={s.item}>Мероприятие</NavLink>
                         <NavLink onClick={() => setShow(!show)} to="/contact" className={s.item}>Контакты</NavLink>
-                        <btn className={`btn btn-info text-center text-white`}><img src='/images/call.png' /> Позвонить нам</btn>
+                        {/* <btn className={`btn btn-info text-center text-white`}><img src='/images/call.png' /> Позвонить нам</btn> */}
+                        <a href="tel:+996701893489" className={`btn btn-info text-center text-white ${s.call} `}><img src='/images/call.png' /> Позвонить нам</a>
 
-                        {/* <btn className={`text-center text-white `}>Позвонить</btn> */}
                     </nav> 
 
                 </div>
